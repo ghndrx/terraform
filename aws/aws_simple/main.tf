5bd7a7562b847caccc7e1c903db4edc082861459
Greg Hendrickson <aisthanestha@gmail.com>
2023-11-17 14:02:43 -0800

    remove efs elb

--- aws/aws_simple/main.tf
+++ aws/aws_simple/main.tf
@@ -62,19 +62,3 @@ module "ec2-west" {
   vpc_id_west_2 =  module.vpc-west.vpc_id_west_2
 }
 
-
-module "elb" {
-  source = "./modules/elb"
-}
-
-module "efs" {
-    source = "./modules/efs"
-    
-    us_east_subnet_1_id = module.subnets_us_east.us_east_subnet_1_id
-    us_east_subnet_2_id = module.subnets_us_east.us_east_subnet_2_id
-    us_east_subnet_3_id = module.subnets_us_east.us_east_subnet_3_id
-    us_west_subnet_1_id = module.subnets_us_west.us_west_subnet_1_id
-    us_west_subnet_2_id = module.subnets_us_west.us_west_subnet_2_id
-    us_west_subnet_3_id = module.subnets_us_west.us_west_subnet_3_id
-    
-}
