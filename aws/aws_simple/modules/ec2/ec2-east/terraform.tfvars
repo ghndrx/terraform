71092a702f3571775660be3c14bd0c80111c0df5
Greg Hendrickson <aisthanestha@gmail.com>
2023-11-17 11:22:13 -0800

    syntax in tfvars. update userdata to base64 encode

--- aws/aws_simple/modules/ec2/ec2-east/terraform.tfvars
+++ aws/aws_simple/modules/ec2/ec2-east/terraform.tfvars
@@ -1,2 +1,2 @@
-min_size = 3
-max_size = 10
+min_size = "3"
+max_size = "10"
--- aws/aws_simple/modules/ec2/ec2-east/variables.tf
+++ aws/aws_simple/modules/ec2/ec2-east/variables.tf
@@ -3,13 +3,11 @@
 variable "min_size" {
     type        = number
     description = "Minimum number of instances in the Auto Scaling Group"
-    default     = 1
 }
 
 variable "max_size" {
     type        = number
     description = "Maximum number of instances in the Auto Scaling Group"
-    default     = 1
 }
 
 
